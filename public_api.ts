93639d93ed58e9c4e586935b7284db02f3b5db6b
keshavm <keshavm@KESHAVM-W10>
2018-12-27 14:55:06 +0530

    Added header component

--- public_api.ts
+++ public_api.ts
@@ -1,3 +1,4 @@
 export * from './src/app/demos/demo/demo.module';
 export { DemoService } from './src/app/demos/demo.service';
 export { DemoComponent } from './src/app/demos/demo/demo.component';
+export { HeaderComponent } from './src/app/demos/header/header.component';
--- src/app/demos/demo/demo.module.ts
+++ src/app/demos/demo/demo.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+
+import { HeaderComponent } from '../header/header.component';
 import { DemoComponent } from './demo.component';
 
 import { DemoService } from '../demo.service';
 import { MyHttpService } from '../myhttpservice.service';
 
 @NgModule({
-  declarations: [DemoComponent],
+  declarations: [DemoComponent, HeaderComponent],
   imports: [CommonModule, HttpClientModule],
-  exports: [DemoComponent],
+  exports: [DemoComponent, HeaderComponent],
   providers: [
     // no need to place providers if `providedIn` flag is added in service.
     DemoService,
